b47260dcf87d76e51fe92d765c4e2a73dfe6831f
FrancescoS <francesco.salerno2294@gmail.com>
2020-07-06 15:45:31 +0200

    End

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var nome = prompt("inserisci il tuo nome");
+// console.log(nome);
+var cognome = prompt("inserisci il tuo cognome")
+// console.log(cognome);
+var colore = prompt("Inserisci il tuo colore preferito")
+// console.log(colore);
+document.getElementById('prova').innerHTML = nome + cognome + colore + "19";
